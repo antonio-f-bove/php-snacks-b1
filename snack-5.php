eec6a5950e36141490e31f69c816c0bc4c9096f3
Antonio Bove <antoniofbove@gmail.com>
2022-03-29 16:43:54 +0200

    snack-5 ok

---
+++ snack-5.php
@@ -0,0 +1,6 @@
+<?php
+
+$par = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate distinctio quibusdam eum ullam, velit, veniam corporis molestias consequuntur, temporibus quas perspiciatis soluta doloremque sint maxime architecto nihil alias dolorem earum saepe atque inventore ea! Aperiam nisi corrupti aut. Unde, magni non at commodi, voluptatem cupiditate quis enim quibusdam rerum accusamus quasi est quas incidunt aperiam aliquam illo fugit repellendus adipisci rem delectus libero? Laudantium voluptas totam, cumque iure, quos autem dolorem dolor quam fuga culpa repudiandae! Quibusdam, illum culpa? Voluptatum consectetur, deleniti qui ea accusamus perferendis sed perspiciatis natus nam accusantium quisquam cum rem cupiditate dolor debitis quasi voluptatibus illum.';
+
+$split_par = explode('. ', $par);
+var_dump($split_par);
