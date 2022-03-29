af3bbbf1a424fc2f9361e50bb9734aee3f4c0fbd
Antonio Bove <antoniofbove@gmail.com>
2022-03-29 15:38:01 +0200

    snack-2 ok

---
+++ snack-2.php
@@ -0,0 +1,34 @@
+<?php
+
+extract($_GET); // $name, $address, $age
+$flag = true;
+
+if (isset($name) && isset($address) && isset($age)) {
+
+  if (strlen($name) < 3) {
+    echo 'Name too short => ';
+    $flag = false;
+  }
+
+  if (substr_count($address, '@') != 1 || !strpos($address, '.', -4)) {
+    // verifica se l'email contiene una sola '@' e se il punto è tra gli ultimi 4 caratteri
+    echo 'invalid email address => ';
+    $flag = false;
+  }
+
+  if (!intval($age)) {
+    echo 'Age is not a number => ';
+    $flag = false;
+  }
+
+  if ($flag) {
+    // se ha passato tutti i controlli
+    echo 'Access GRANTED';
+  } else {
+    echo 'Access DENIED';
+  }
+
+} else {
+
+  echo 'missing arguments';
+}
